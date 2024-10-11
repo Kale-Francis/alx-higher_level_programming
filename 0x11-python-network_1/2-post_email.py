05dc8c9683639080b67bca24ae0582ea9fd2b34b
Kale-Francis <kalefrancis989@gmail.com>
2024-10-11 18:27:32 +0300

    updated

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
-"""
-Sends a POST request with an email to a URL and displays the response body.
-"""
-
 import urllib.request
 import urllib.parse
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
+url = sys.argv[1]
+email = sys.argv[2]
 
-    values = {'email': email}
-    data = urllib.parse.urlencode(values).encode('utf-8')
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
-    with urllib.request.urlopen(url, data) as response:
-        response_body = response.read().decode('utf-8')
-        print("Your email is:", email)
-        print(response_body)
+with urllib.request.urlopen(url, data=data) as response:
+    print(response.read().decode('utf-8'))
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,14 +3,15 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) < 2 else sys.argv[1]
-    payload = {'q': letter}
-    response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
-    
+    url = 'http://0.0.0.0:5000/search_user'
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    data = {'q': letter}
+    response = requests.post(url, data=data)
+
     try:
-        json_data = response.json()
-        if json_data:
-            print("[{}] {}".format(json_data.get('id'), json_data.get('name')))
+        json_response = response.json()
+        if json_response:
+            print(f"[{json_response.get('id')}] {json_response.get('name')}")
         else:
             print("No result")
     except ValueError:
